9a62b4be939915646c4bae711ec4810308bf1bf0
pmendez2706 <pmendez2706@gmail.com>
2020-06-30 22:34:17 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // JUEGO
 var quiz = [
     {
-      pregunta: 'Cuál es el nombre del planeta rojo?',
+      pregunta: 'Cuál es el nombre del planeta amarillo?',
       respuesta: 'marte',
     },
     {
